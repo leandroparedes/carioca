ca928ed65dbbbab2bff5f81ebad1190040bd64bb
Leandro Paredes <leandroparedes6@gmail.com>
2020-01-18 03:09:50 -0300

    fix color

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div id="app" class="bg-gray-800 text-gray-300 min-h-screen">
         <router-view/>
 
-        <div class="text-center bg-gray-700" v-if="this.$store.state.players.length > 0 || this.$store.state.games.length > 0">
+        <div class="text-center" v-if="this.$store.state.players.length > 0 || this.$store.state.games.length > 0">
              <button @click="reset" class="bg-red-500 text-white px-2 rounded mt-2 mb-8">Reset</button>
          </div>
     </div>
