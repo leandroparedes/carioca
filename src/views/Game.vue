6d1ff90c0891c1c7d8a8186063bd82609d8c88b5
Leandro Paredes <leandroparedes6@gmail.com>
2020-01-20 20:17:41 -0300

    updates on game logic

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,15 +1,19 @@
 <template>
-    <div class="px-10 pb-10 text-center">
-        <h1 class="text-blue-500 text-4xl font-semibold py-5">
-            {{ currentGame.name }}
-        </h1>
+    <div>
+        <button @click="endTurn" class="min-h-screen min-w-full focus:outline-none">
+            <div class="-mt-64">
+                <h1 class="text-blue-500 text-4xl font-semibold py-5">
+                    {{ currentGame.name }}
+                </h1>
 
-        <div>
-            <div class="text-gray-400 text-xl">Jugando</div>
-            <div class="text-5xl font-bold text-green-400">{{ currentPlayer.name.toUpperCase() }}</div>
-        </div>
+                <div>
+                    <div class="text-gray-400 text-xl">Jugando</div>
+                    <div class="text-5xl font-bold text-green-400">{{ currentPlayer.name.toUpperCase() }}</div>
+                </div>
 
-        <timer :time="currentPlayer.timeLeft" :init="true" />
+                <timer :time="currentPlayer.timeLeft" :init="true" @timeUpdated="setUpdatedTime"/>
+            </div>
+        </button>
     </div>
 </template>
 
@@ -29,15 +33,40 @@ export default {
         }
     },
     components: { Timer },
+    data: function () {
+        return {
+            currentGame: this.$store.getters.gameById(this.$store.state.currentGameId),
+            currentPlayer: this.$store.getters.playerById(this.$store.state.currentPlayerId),
+            currentTime: 0
+        };
+    },
     created () {
-        this.$store.commit('set_current_game', this.$route.params.id);
-        this.$store.commit('clear_next_game');
-        
-        this.$store.commit('set_current_player', this.currentPlayer ? this.currentPlayer.id : this.$store.state.nextPlayer.id);
-        this.$store.commit('clear_next_player');
-
         this.$store.dispatch('set_timers', this.currentGame.time);
     },
-    computed: mapState(['currentGame', 'currentPlayer'])
+    mounted () {
+        window.addEventListener('beforeunload', this.leaving);
+    },
+    beforeDestroy () {
+        window.removeEventListener('beforeunload', this.leaving);
+    },
+    methods: {
+        leaving: function () {
+            this.saveTimeCurrentPlayer();
+        },
+        setUpdatedTime: function (updatedTime) {
+            this.currentTime = updatedTime;
+        },
+        saveTimeCurrentPlayer: function () {
+            this.$store.commit('set_timer_for_player', {
+                playerId: this.currentPlayer.id,
+                time: this.currentTime
+            });
+        },
+        endTurn: function () {
+            this.saveTimeCurrentPlayer();
+
+            // change to next player
+        }
+    }
 }
 </script>
