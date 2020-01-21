c8f1a4808736c487a63c9f4531a2422c8debf913
Leandro Paredes <leandroparedes6@gmail.com>
2020-01-21 17:27:37 -0300

    go back

--- src/views/GameResult.vue
+++ src/views/GameResult.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>game results</div>
+    <div class="px-10 pb-10 text-center">
+        <h1 class="text-blue-500 text-4xl font-semibold pt-2 pb-5">
+            Resultados
+        </h1>
+        <button class="text-left text-red-500 font-semibold pt-2" @click.once="$router.go(-1);">Volver atrás</button>
+    </div>
 </template>
 
 <script>
