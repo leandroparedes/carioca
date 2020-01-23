afebbd428921db2bc4911e3e30c56d099e1f0ad8
Leandro Paredes <leandroparedes6@gmail.com>
2020-01-23 01:46:02 -0300

    fix typo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(__dirname + "/dist/"));
 
 app.get(/.*/, function (req, res) {
-    res.sendfile(_-__dirname + "/dist/index.html");
+    res.sendfile(__dirname + "/dist/index.html");
 });
 
 const port = process.env.PORT || 8080;
