64e2feda0dad3486a2ac3a486a8af6c083ffff77
Leandro Paredes <leandroparedes6@gmail.com>
2020-01-23 18:46:42 -0300

    redo game result view

--- src/views/GameResult.vue
+++ src/views/GameResult.vue
@@ -4,47 +4,73 @@
             Resultados
         </h1>
 
-        <div class="text-center text-lg text-gray-400 font-semibold mb-10">
+        <div class="text-center text-lg text-gray-500 font-semibold mb-4">
             Juego {{ currentGame.id }} &#183; {{ currentGame.name }}
         </div>
 
-        <div class="px-10">
-            <div
-                v-for="player in $store.state.players" :key="player.id"
-                class="flex justify-between mb-6 font-semibold"
-            >
-                <div
-                    class="w-4/6 text-2xl"
-                    :class="{'text-green-500': scores[player.id] == 0}"
+        <div class="px-8">
+            <div class="font-semibold text-green-500 text-lg mb-2">Ganador</div>
+
+            <select v-model="selected" class="w-full block input input-lg mb-8 focus:outline-none" @change="changeSelect">
+                <option
+                    v-for="player in $store.state.players" :key="player.id"
+                    :value="player.id"
+                    selected
                 >
+                    {{ player.name }}
+                </option>
+            </select>
+
+            <div class="flex justify-between text-gray-500 mb-1">
+                <div class="w-4/6">Jugador</div>
+                <div class="w-2/6 text-center">Puntos</div>
+            </div>
+            <hr class="mb-3 border-1 border-gray-500">
+
+            <div class="flex justify-between font-semibold mb-5 text-green-500">
+                <div class="w-4/6 text-2xl">{{ selectedPlayer.name }}</div>
+                <div class="w-2/6 text-2xl text-center">0</div>
+            </div>
+
+            <div
+                v-for="player in playersWithoutSelected" :key="player.id"
+                class="flex justify-between mb-5 font-semibold"
+            >
+                <div class="w-4/6 text-2xl">
                     {{ player.name }}
                 </div>
 
                 <input
-                    type="number" min="0"
+                    type="number" min="1"
                     v-model.number="scores[player.id]"
+                    @change="errorMessage = null"
                     class="w-2/6 text-center input input-lg"
                 >
             </div>
-        </div>
 
-        <div v-if="error" class="text-center text-red-500 font-semibold mt-8">{{ error }}</div>
-
-        <div class="flex flex-col mt-5 px-10">
-            <button
-                @click="continueGame"
-                class="btn btn-lg btn-green"
-                :disabled="loading"
+            <div
+                v-if="errorMessage"
+                class="text-red-500 font-semibold text-lg text-center"
             >
-                {{ loading ? 'Guardando' : 'Continuar' }}
-            </button>
+                {{ errorMessage }}
+            </div>
 
-            <button
-                @click.once="$router.go(-1)"
-                class="text-blue-500 text-xl font-semibold text-center mt-5"
-            >
-                Volver atrás
-            </button>
+            <div class="flex flex-col mt-8">
+                <button
+                    @click="continueGame"
+                    class="btn btn-lg btn-green"
+                    :disabled="loading"
+                >
+                    {{ loading ? 'Guardando' : 'Continuar' }}
+                </button>
+
+                <button
+                    @click.once="$router.go(-1)"
+                    class="text-blue-500 text-lg font-semibold text-center mt-6"
+                >
+                    Volver atrás
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -53,54 +79,54 @@
 export default {
     data: function () {
         return {
+            selected: this.$store.state.currentPlayerId,
             currentGame: this.$store.getters.gameById(this.$store.state.currentGameId),
             scores: {},
             loading: false,
-            error: null
+            errorMessage: null
         };
     },
     methods: {
-        continueGame: function () {
-            if (this.saveResults()) {
-                this.setNextPlayer();
-                this.nextGameOrResults();
-            }
+        changeSelect: function () {
+            this.scores = {};
         },
-        saveResults: function () {
+        continueGame: function () {
+            this.errorMessage = null;
             this.loading = true;
-            this.error = null;
 
-            if (Object.entries(this.scores).length < this.$store.state.players.length) {
+            // save the result
+            let scoresValues = Object.entries(this.scores);
+
+            if (scoresValues.length && scoresValues.length == this.playersWithoutSelected.length) {
+                let validData = true;
+                scoresValues.some(score => {
+                    if (!score[1] || score[1] < 1 || !Number.isInteger(score[1])) {
+                        validData = false;
+                        return;
+                    }
+                });
+                if (!validData) {
+                    this.loading = false;
+                    this.errorMessage = 'Hay un problema con los resultados ingresados';
+                    return;
+                }
+            } else {
                 this.loading = false;
-                return false;
+                this.errorMessage = 'Hay un problema con los resultados ingresados'
+                return;
             }
 
-            let values = [];
-
-            Object.entries(this.scores).forEach(score => {
-                values.push(score[1]);
-            });
-
-            let filtered = values.filter(v => v <= 0);
-
-            if (filtered.length == 0 || filtered.length > 1) {
-                this.error = "Solamente un jugador debe tener score 0."
-                this.loading = false;
-                return false;
-            }
+            let score = Object.assign({}, this.scores);
+            score[this.selectedPlayer.id] = 0;
 
             let result = {
                 gameId: this.$store.state.currentGameId,
-                scores: this.scores
+                scores: score
             };
 
             this.$store.dispatch('save_result', result);
 
-            this.$store.dispatch('clear_timers');
-
-            return true;
-        },
-        setNextPlayer: function () {
+            // set the next player
             const nextRotatingPlayer = this.$store.getters.playerById(this.$store.state.rotatingPlayerId + 1);
 
             if (nextRotatingPlayer) {
@@ -110,8 +136,8 @@ export default {
                 this.$store.commit('set_rotating_player', 1);
                 this.$store.commit('set_current_player', 1);
             }
-        },
-        nextGameOrResults: function () {
+
+            // set the next game
             const nextGame = this.$store.getters.gameById(this.$store.state.currentGameId + 1);
 
             if (nextGame) {
@@ -121,6 +147,14 @@ export default {
                 this.$router.push('/results');
                 this.$store.commit('game_over');
             }
+        }
+    },
+    computed: {
+        selectedPlayer: function () {
+            return this.$store.getters.playerById(this.selected);
+        },
+        playersWithoutSelected: function () {
+            return this.$store.state.players.filter(player => player.id != this.selected);
         }
     }
 }
